e06736a5bad4cbeb7ea703f6825e2a6e77f350c5
Dave Warnock <dwarnock@gmail.com>
2024-09-25 12:29:14 +0100

    added more tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,4 @@
-from pyPrayerOfHannah.db import DB, Author
+from pyPrayerOfHannah.db import DB, Author, Song_Book
 from sqlmodel import SQLModel, Field, create_engine, Session, Relationship, select
 from sqlmodel._compat import SQLModelConfig
 from sqlalchemy import Column, String, Index, engine
@@ -14,24 +14,50 @@ def test_delete_database_test() -> None:
 
 def test_add_author() -> None:
     d = DB()
+    d.delete_database()
     d.create_database_structure()
     d.get_session()
 
     with Session(d.engine) as session:
 
-        a_wesley_john: Author = Author(surname="Wesley", firstnames="John")
-        session.add(a_wesley_john)
+        row: Author = Author(surname="Wesley", firstnames="John")
+        session.add(row)
 
         session.commit()
 
-        if a_wesley_john.id != 1: 
-            raise AssertionError(f"Add author id should be 1: {a_wesley_john.id}")
+        if row.id != 1: 
+            raise AssertionError(f"Add id should be 1: {row.id}")
 
-        if a_wesley_john.surname != "Wesley": 
-            raise AssertionError(f"Add author surname should be 'Wesley': {a_wesley_john.surname}")
+        if row.surname != "Wesley": 
+            raise AssertionError(f"Add author surname should be 'Wesley': {row.surname}")
 
-        if a_wesley_john.firstnames != "John": 
-            raise AssertionError(f"Add author firstnames should be 'John': {a_wesley_john.firstnames}")
+        if row.firstnames != "John": 
+            raise AssertionError(f"Add author firstnames should be 'John': {row.firstnames}")
 
-        if a_wesley_john.display_name != "Wesley, John": 
-            raise AssertionError(f"Add author displayname should be 'Wesley, John': {a_wesley_john.display_name}")
+        if row.display_name != "Wesley, John": 
+            raise AssertionError(f"Add author displayname should be 'Wesley, John': {row.display_name}")
+
+def test_add_song_book() -> None:
+    d = DB()
+    d.delete_database()
+    d.create_database_structure()
+    d.get_session()
+
+    with Session(d.engine) as session:
+
+        row: Song_Book = Song_Book(code="StF", name="Singing the Faith", url="methodist.org")
+        session.add(row)
+
+        session.commit()
+
+        if row.id != 1: 
+            raise AssertionError(f"Add id should be 1: {row.id}")
+
+        if row.code != "StF": 
+            raise AssertionError(f"Add code should be 'StF': {row.code}")
+
+        if row.name != "Singing the Faith": 
+            raise AssertionError(f"Add name should be 'Singing the Faith': {row.name}")
+
+        if row.url != "methodist.org": 
+            raise AssertionError(f"Add url should be 'methodist.org': {row.url}")
