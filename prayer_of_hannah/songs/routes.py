81c53bed1497793dd65020cdfb33cfd5613aabca
Dave Warnock <dwarnock@gmail.com>
2024-10-17 01:13:58 +0100

    Song list via htmx

--- prayer_of_hannah/songs/routes.py
+++ prayer_of_hannah/songs/routes.py
@@ -4,8 +4,12 @@ from prayer_of_hannah.songs import bp
 from prayer_of_hannah.models import Song
 from prayer_of_hannah import get_dbe
 
-@bp.route('/')
+@bp.get('/')
 def index():
+    return render_template('songs/index.html')
+
+@bp.get('/htmx/songs')
+def songs():
     with Session(get_dbe()) as session:
         songs = session.exec(select(Song).order_by(Song.title))
-        return render_template('songs/index.html', songs = songs)
+        return render_template('songs/songs.html', songs = songs)
